058ebb9d8918b52dae4014dbc6c3c7be19c4edc1
TIEN TRUNG <ngtientrung31@gmail.com>
2023-12-29 23:37:16 +0700

    Trung changed

--- app/src/main/java/com/example/ihealthdroid/AccountCreatingActivity.kt
+++ app/src/main/java/com/example/ihealthdroid/AccountCreatingActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
