3919f3a594257c576d4631cc0767398ddad40776
Lynx1032 <hohoangnghiep.2002@gmail.com>
2024-01-10 22:13:20 +0700

    Make some changes at Medical Profile page

--- app/src/main/java/com/example/ihealthdroid/MainActivity.kt
+++ app/src/main/java/com/example/ihealthdroid/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
