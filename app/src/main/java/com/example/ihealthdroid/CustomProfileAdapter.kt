277122b14ddc1e59667bbafef38e6b0fd6f9a446
TIEN TRUNG <ngtientrung31@gmail.com>
2024-01-11 22:21:31 +0700

    Add button to Adapter

--- app/src/main/java/com/example/ihealthdroid/CustomProfileAdapter.kt
+++ app/src/main/java/com/example/ihealthdroid/CustomProfileAdapter.kt
@@ -1,17 +1,26 @@
 package com.example.ihealthdroid
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ihealthdroid.ui.theme.IHealthDroidTheme
 
 class CustomProfileAdapter : ListAdapter<ProfileModel, CustomProfileAdapter.ProfileViewHolder>(DiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.medical_profile_recycle_addon, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_profile, parent, false)
         return ProfileViewHolder(view)
     }
 
@@ -21,14 +30,22 @@ class CustomProfileAdapter : ListAdapter<ProfileModel, CustomProfileAdapter.Prof
     }
 
     inner class ProfileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val userNameView: TextView = itemView.findViewById(R.id.userNameView)
-        private val userPhoneView: TextView = itemView.findViewById(R.id.userPhoneView)
-
+        private val userNameView: TextView = itemView.findViewById(R.id.tv_show_name)
+        private val userPhoneView: TextView = itemView.findViewById(R.id.tv_show_date)
 
         fun bind(profile: ProfileModel) {
             userNameView.text = profile.name
             userPhoneView.text = profile.phone
         }
+
+        val btnDetail: Button = itemView.findViewById(R.id.btn_detail)
+        init {
+            btnDetail.setOnClickListener {
+                val intent = Intent(itemView.context, ProfileDetailActivity::class.java)
+                itemView.context.startActivity(intent)
+            }
+        }
+
     }
 
     private class DiffCallback : DiffUtil.ItemCallback<ProfileModel>() {
@@ -40,4 +57,5 @@ class CustomProfileAdapter : ListAdapter<ProfileModel, CustomProfileAdapter.Prof
             return oldItem == newItem
         }
     }
+
 }
